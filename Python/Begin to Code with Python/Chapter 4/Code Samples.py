de89fc3e9b38de2e9ebbd747ae93623a316daa1f
Aubrey Smith <aubrey.smith@orcon.net.nz>
2019-11-29 10:08:29 +1300

    Updated scripts for chapter4

--- Python/Begin to Code with Python/Chapter 4/Code Samples.py
+++ Python/Begin to Code with Python/Chapter 4/Code Samples.py
@@ -1,1 +1,22 @@
 # Chapter 4
+
+print("It's a trap!")
+
+print('''...and then Luke said, "It's a trap"''')
+
+print('hello\nworld')
+
+print('Item\tSales\nCar\t50\nBoat\t10')
+
+print('...and then Luke said “It’s a trap.”')
+
+name=input('Enter your name please: ')
+
+input('Press enter to continue')
+
+print(name)
+
+# eval() Evaluates any string
+
+time_text = input('Enter the cooking time in seconds: ')
+time_int = int(time_text)
