9ba33a31e248d81706ca7b4a250d50fadeea833b
Aubrey Smith <aubrey.smith@orcon.net.nz>
2020-06-11 20:50:18 +1200

    Update Python source

--- Python/Artifactory/RestAPI (2).py
+++
@@ -1,6 +0,0 @@
-import requests
-requests.get('https://google.com')
-print(response.status_code)
-
-print (type(response.json()) )
-print(response.json())
--- Python/Artifactory/Scripts/pip_cacaerts.ps1
+++ Python/Artifactory/Scripts/pip_cacaerts.ps1
@@ -1,4 +1,4 @@
-pip config set global.cert D:\Artifactory\API\Python\cacert.pem
+pip config set global.cert D:\Code\Python\Artifactory\Config\cacert.pem
 
 
 # Use -v to show where your pip config file is
