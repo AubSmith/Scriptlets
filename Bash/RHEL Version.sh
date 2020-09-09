f2f501836342c8113993ba826640a572eead59cc
Smith, Aubrey <Aubrey.Smith@outlook.com>
2020-09-10 06:49:07 +1200

    Added RedHat version commands

---
+++ Bash/RHEL Version.sh
@@ -0,0 +1,10 @@
+hostnamectl
+
+cat /etc/redhat-release
+
+rpm --query centos-release
+rpm --query redhat-release
+
+cat /etc/issue
+
+cat /etc/system-release-cpe
---
+++ Bash/SetIP.sh
@@ -0,0 +1,13 @@
+# Set static IP
+sudo ifconfig eth0 172.29.120.155 netmask 255.255.255.0
+
+# Del static IP
+ip addr del 1.1.1.2/32 dev eth2
+ifconfig en1 delete 192.168.141.99
+
+# Reset DHCP
+sudo dhclient -r
+
+ifdown eth0
+
+ifup eth0
