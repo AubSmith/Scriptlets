3e0e50d7e9e5bfceb5c7bf950e8dfa89b3197392
Smith, Aubrey <Aubrey.Smith@outlook.com>
2021-05-23 20:58:50 +1200

    Updated Linux Fundamentals notes

---
+++ Bash/Linux Fundamentals/Module 1 - Essential Commands/Lesson 3 - Essential File Management Tools/3.5 Working with Directories.sh
@@ -0,0 +1,14 @@
+cd /tmp
+pwd
+cd -
+cd tmp
+pwd
+mkdir data
+cd /data
+cd data
+mkdir files/morefile
+ls
+mkdir -p files/morefile
+ls -R
+rmdir data # Removes dir - only if empty
+rm -rf data`
---
+++ Bash/Linux Fundamentals/Module 1 - Essential Commands/Lesson 3 - Essential File Management Tools/3.6 Using Absolute and Relative Paths.sh
@@ -0,0 +1,18 @@
+ls -ld hosts data data/passwd
+
+cd data
+ls
+cp passwd ..
+cp ../hosts .
+
+cd /usr/share/doc/
+ls
+cd pam-1.1.8/
+ls
+cd html
+ls
+pwd
+cp sag-author.html ../../../../../tmp
+cp sag-author.html /tmp
+
+ls -a
