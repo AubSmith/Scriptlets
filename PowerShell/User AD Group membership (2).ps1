31876dd847491627da1b91c6cb93ca3eceb1a4b7
Smith, Aubrey <Aubrey.Smith@outlook.com>
2021-06-01 11:38:38 +1200

    Updating PS scripts

--- PowerShell/User AD Group membership (2).ps1
+++
@@ -1,3 +0,0 @@
-﻿$User = '%username%'
-
-(New-Object System.DirectoryServices.DirectorySearcher("(&(objectCategory=User)(samAccountName=$User))")).FindOne().GetDirectoryEntry().memberOf | Out-File C:\Groups.txt
