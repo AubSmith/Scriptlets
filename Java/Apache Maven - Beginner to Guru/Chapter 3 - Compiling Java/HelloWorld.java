2cbae93385b9cec73431a0378e19b4185dad2104
Smith, Aubrey <Aubrey.Smith@outlook.com>
2021-06-23 06:57:02 +1200

    Updated Maven study notes

--- Java/Apache Maven - Beginner to Guru/Chapter 3 - Compiling Java/HelloWorld.java
+++ Java/Apache Maven - Beginner to Guru/Chapter 3 - Compiling Java/HelloWorld.java
@@ -3,6 +3,6 @@ import org.apache.commons.lang3.StringUtils;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(StringUtils.capitalize("hello world"));
+        System.out.println(StringUtils.capitalize( str: "hello world"));
     }
 }
