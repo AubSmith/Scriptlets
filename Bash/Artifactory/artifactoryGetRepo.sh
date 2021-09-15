68662002a0b76df9c3bf10db0a0d417d8ea80766
Smith, Aubrey <Aubrey.Smith@outlook.com>
2021-09-16 07:55:14 +1200

    Artifactory API getRepo curl update

--- Bash/Artifactory/artifactoryGetRepo.sh
+++ Bash/Artifactory/artifactoryGetRepo.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 curl -uusername:token https://artifactory.com/artifactory/api/repositories?packageType=rpm
 curl -uusername:token https://artifactory.com/artifactory/api/repositories?type=local
 
