fa31575c6052b6fe79122e2c1e859831c9dcd16a
Ethan Smith <Aubrey.Smith@orcon.net.nz>
2022-03-28 07:51:37 +1300

    PowerShell Update

---
+++ PowerShell/Get-Process.ps1
@@ -0,0 +1,12 @@
+Get-Process explorer.exe
+
+Get-Process * -ComputerName "$Env:Computername"
+
+Get-Process -Id "4612"
+
+Get-Help Get-Process
+
+# Run as administrator
+Update-Help
+
+Get-Help Get-Process -Online
