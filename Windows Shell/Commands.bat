580fa3902270d47ee215bf0b2a513c574ded4060
Ethan Smith <Aubrey.Smith@orcon.net.nz>
2022-04-11 10:37:00 +1200

    vscode

--- Windows Shell/Commands.bat
+++ Windows Shell/Commands.bat
@@ -6,15 +6,3 @@ type filename.txt
 
 # Logged in user
 query user /server:computername
-
-# Redo
-# Ctrl + Shift + Z
-
-# Switch line
-# Alt + Up/Down
-
-# Multiple cursors
-# Alt + left click
-
-# Open file fast
-# Ctrl + P then filename
---
+++ Windows Shell/vscode.md
@@ -0,0 +1,11 @@
+# Redo
+# Ctrl + Shift + Z
+
+# Switch line
+# Alt + Up/Down
+
+# Multiple cursors
+# Alt + left click
+
+# Open file fast
+# Ctrl + P then filename
