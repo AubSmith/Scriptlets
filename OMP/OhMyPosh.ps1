9ba0da902ad8d735fb564eb38d0e8ffa7db21be6
Ethan Smith <Aubrey.Smith@orcon.net.nz>
2023-03-14 21:26:32 +1300

    Terminal updates

---
+++ OMP/OhMyPosh.ps1
@@ -0,0 +1,31 @@
+winget install JanDeDobbeleer.OhMyPosh -s winget
+
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+# Test PowerShell profile path
+Test-Path $Profile
+
+# Create PowerShell profile
+New-Item –Path $Profile –Type File –Force
+
+# Add to PowerShell profile
+# oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+
+# Add nerd font as required
+
+oh-my-posh font install
+
+
+# New instructions
+# PowerShell
+winget install oh-my-posh
+
+notepad $Profile
+# Update PS profile
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Install fonts
+# Load NF in Terminal
+
+# Linux
