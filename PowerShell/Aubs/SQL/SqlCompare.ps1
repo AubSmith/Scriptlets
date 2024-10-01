eb023444d84c19b6f84e182012a2aa497f481704
Ethan Smith <Aubrey.Smith@orcon.net.nz>
2024-10-01 20:58:59 +1300

    SQLCompare update

--- PowerShell/Aubs/SQL/SqlCompare.ps1
+++ PowerShell/Aubs/SQL/SqlCompare.ps1
@@ -10,19 +10,51 @@ Import-Module SqlServer
 function Get-Environment {
     # Prompt for environment selection
     $environment = Read-Host -Prompt "Select the environment to compare:
-                                      Production
+                                      Prod
                                       OAT
                                       SIT
-                                      DEV"
+                                      DEV
+Enter your environment"
 
-    return $environment
+	# Set variables based on the environment
+	switch ($environment) {
+	    "Prod" {
+	        $instance = "DESKTOP-EM8HVC2"
+	        $dbName = "AdventureWorks2022"
+	    }
+	    "OAT" {
+	        $instance = "DESKTOP-EM8HVC2"
+	        $dbName = "AdventureWorks2019"
+	    }
+	    "SIT" {
+	        $instance = "SITServer"
+	        $dbName = "SITDatabase"
+	    }
+	    "DEV" {
+	        $instance = "DEVServer"
+	        $dbName = "DEVDatabase"
+	    }
+	    default {
+	        Write-Output "Invalid environment selected. Please select one of the following:
+	                      Prod
+	                      OAT
+	                      SIT
+	                      DEV
+
+						 Environment: "
+
+	        exit
+	    }
+	}
+
+    return $environment, $instance, $dbName
 }
 
 
 # Define the function to get credentials
 function Get-Credentials {
     $username = Read-Host -Prompt "Use downlevel login in the form: domain\username
-                                   Enter your username"
+Enter your username"
 
     $password = Read-Host -AsSecureString -Prompt "Enter your password"
 
@@ -31,31 +63,35 @@ function Get-Credentials {
 
 
 # Define the function to query a SQL database
-function Query-Sql {
+function Get-SqlQuery {
 	param (
         [string]$instance,
-        [string]$databaseName,
+        [string]$dbName,
 		[string]$username,
-		[string]$password,
-        [string]$query
+		[securestring]$password,
+		[string]$environment
 	)
 
     Write-Output "Connecting to to SQL..."
 
     # Define the connection string
-    $connectionString = 'Server=$server;Database=$databaseName;UID=$username;PWD=$password;Integrated Security=true;'
+    # $connectionString = "Server=$instance; Database=$dbName; UID=$username; PWD=$password; Integrated Security=true;"
+	$connectionString = "Server=$server; Database=$databaseName; Integrated Security=true; TrustServerCertificate=True;"
 
     # Define the SQL query
-    $query = Get-Content -Path .\SqlQuery.sql
+    $query = "SELECT TOP (5) [LocationID]
+		,[Name]
+		,[CostRate]
+		,[Availability]
+		,[ModifiedDate]
+	FROM [AdventureWorks2022].[Production].[Location]
+	ORDER BY LocationID ASC;"
 
     # Execute the query
-    Invoke-Sqlcmd -ConnectionString $connectionString -Query $query
-
-    # Define the output file path
-    $outputFilePath = "C:\path\to\output.txt"
+    $result = Invoke-Sqlcmd -ConnectionString $connectionString -Query $query
 
     # Write the results to the file
-    $result | Out-File -FilePath $outputFilePath
+    $result | Export-Csv -Path ".\$environment.csv" -NoTypeInformation
 
 }
 
@@ -63,14 +99,14 @@ function Query-Sql {
 # Define the function to compare two files
 function Compare-Files {
 	param (
-		[string]$extractEnvironment1,
-		[string]$extractEnvironment2,
-		[string]$LogFilePath = "Difference_Log.txt"
+		[string]$firstEnv,
+		[string]$secondEnv,
+		[string]$LogFilePath = ".\Difference_Log.txt"
 	)
 
 	# Read the contents of the files into arrays
-	$file1Content = Get-Content -Path $extractEnvironment1
-	$file2Content = Get-Content -Path $extractEnvironment2
+	$file1Content = Get-Content -Path ".\$firstEnv.csv"
+	$file2Content = Get-Content -Path ".\$secondEnv.csv"
 
 	# Find lines that are in the first file but not in the second (removed lines)
 	$removedLines = $file1Content | Where-Object { $_ -notin $file2Content }
@@ -80,10 +116,10 @@ function Compare-Files {
 
 	# Write the differences to the log file
 	$logContent = @()
-	$logContent += "Removed Lines:"
+	$logContent += "Rows in $firstEnv not in $secondEnv :"
 	$logContent += $removedLines
 	$logContent += ""
-	$logContent += "Added Lines:"
+	$logContent += "Rows in $secondEnv not in $firstEnv :"
 	$logContent += $addedLines
 
 	$logContent | Out-File -FilePath $LogFilePath
@@ -93,9 +129,29 @@ function Compare-Files {
 
 
 try {
-    $environment1 = Get-Environment()
-    
-    $username1, $password1 = Get-Credentials()
+    $environment, $instance, $dbName = Get-Environment
+	
+    $username, $password = Get-Credentials
+
+	Get-SqlQuery $instance $dbName $username $password $environment
+
+	$firstEnv = $environment
+
+}
+catch {
+    Write-Output $_
+    Exit 1
+}
+
+try {
+    $environment, $instance, $dbName = Get-Environment
+	
+    $username, $password = Get-Credentials
+
+	Get-SqlQuery $instance $dbName $username $password $environment
+
+	$secondEnv = $environment
+
 }
 catch {
     Write-Output $_
@@ -103,48 +159,10 @@ catch {
 }
 
 
-
-
-
-# Set variables based on the environment
-switch ($environment) {
-    "Production" {
-        $instance = "DESKTOP-EM8HVC2"
-        $databaseName = "AdventureWorks2022"
-    }
-    "OAT" {
-        $instance = "OATServer"
-        $databaseName = "OATDatabase"
-    }
-    "SIT" {
-        $instance = "SITServer"
-        $databaseName = "SITDatabase"
-    }
-    "DEV" {
-        $instance = "DEVServer"
-        $databaseName = "DEVDatabase"
-    }
-    default {
-        Write-Output "Invalid environment selected. Please select one of the following:
-                      Production
-                      OAT
-                      SIT
-                      DEV"
-        exit
-    }
+try {
+	Compare-Files $firstEnv $secondEnv
 }
-
-
-
-
-<#
-select environment 1
-    get creds
-    set vars
-
-select environment 2
-    get creds
-    set vars
-
-query sql
-#>
+catch {
+	Write-Output $_
+    Exit 1	
+}
